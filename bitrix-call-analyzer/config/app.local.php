12a89fe8dbfadf7158f1d2cd9f2a445bba7a0caf
Floi807 <pudenkokoka@gmail.com>
2026-04-13 19:39:01 +0200

    Add files via upload

---
+++ bitrix-call-analyzer/config/app.local.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'app_url' => 'https://8a01-216-106-187-181.ngrok-free.app',
+    'client_id' => 'local.XXXXXXXXXXXXXXXXXXXXXXXX',
+    'client_secret' => 'replace-me',
+    'webhook_url' => 'https://b24-pplyid.bitrix24.ru/rest/1/qytinepa4bq2jdmw',
+    'verify_ssl' => false,
+    'scope' => 'crm,catalog,placement',
+    'storage_path' => dirname(__DIR__) . '/data',
+    'log_file' => dirname(__DIR__) . '/data/app.log',
+    'transcript_sources' => [
+        'deal_fields' => [
+            'UF_CRM_CALL_TRANSCRIPT',
+        ],
+        'activity_types' => [2],
+    ],
+    'catalog' => [
+        'aliases' => [
+            'окно стандарт' => ['стандартное окно'],
+            'дверь входная' => ['входная дверь'],
+        ],
+    ],
+    'parser' => [
+        'default_quantity' => 1,
+        'max_products_per_request' => 25,
+    ],
+];
